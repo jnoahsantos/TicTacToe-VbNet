a78f0293a8ba061649118bd19a15a0af764c4c05
jnoahsantos <jamesnoah.santos27@gmail.com>
2022-05-28 20:47:23 +0800

    Complete Tictactoe

---
+++ TICTACTOE/TICTACTOE/Form1.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnGroup = New System.Windows.Forms.GroupBox()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnGroup.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.Color.LavenderBlush
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.No
+        Me.TextBox1.Font = New System.Drawing.Font("Unispace", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.Purple
+        Me.TextBox1.Location = New System.Drawing.Point(8, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(374, 45)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "TIC TAC TOE"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Thistle
+        Me.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn1.Enabled = False
+        Me.btn1.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(6, 283)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(126, 138)
+        Me.btn1.TabIndex = 1
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Thistle
+        Me.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn2.Enabled = False
+        Me.btn2.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(130, 283)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(126, 138)
+        Me.btn2.TabIndex = 2
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Thistle
+        Me.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn3.Enabled = False
+        Me.btn3.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btn3.Location = New System.Drawing.Point(255, 283)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(126, 138)
+        Me.btn3.TabIndex = 3
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Thistle
+        Me.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn4.Enabled = False
+        Me.btn4.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn4.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(6, 147)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(126, 138)
+        Me.btn4.TabIndex = 4
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Thistle
+        Me.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn5.Enabled = False
+        Me.btn5.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn5.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(130, 147)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(126, 138)
+        Me.btn5.TabIndex = 5
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Thistle
+        Me.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn6.Enabled = False
+        Me.btn6.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn6.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(255, 147)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(126, 138)
+        Me.btn6.TabIndex = 6
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Thistle
+        Me.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn7.Enabled = False
+        Me.btn7.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn7.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn7.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(6, 11)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(126, 138)
+        Me.btn7.TabIndex = 7
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Thistle
+        Me.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn8.Enabled = False
+        Me.btn8.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn8.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn8.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(130, 11)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(126, 138)
+        Me.btn8.TabIndex = 8
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Thistle
+        Me.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn9.Enabled = False
+        Me.btn9.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btn9.FlatAppearance.CheckedBackColor = System.Drawing.Color.Magenta
+        Me.btn9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid
+        Me.btn9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn9.Font = New System.Drawing.Font("Arial Rounded MT Bold", 84.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(255, 11)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(126, 138)
+        Me.btn9.TabIndex = 9
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btnGroup
+        '
+        Me.btnGroup.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnGroup.BackColor = System.Drawing.Color.Transparent
+        Me.btnGroup.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnGroup.Controls.Add(Me.btn7)
+        Me.btnGroup.Controls.Add(Me.btn1)
+        Me.btnGroup.Controls.Add(Me.btn9)
+        Me.btnGroup.Controls.Add(Me.btn2)
+        Me.btnGroup.Controls.Add(Me.btn8)
+        Me.btnGroup.Controls.Add(Me.btn3)
+        Me.btnGroup.Controls.Add(Me.btn4)
+        Me.btnGroup.Controls.Add(Me.btn6)
+        Me.btnGroup.Controls.Add(Me.btn5)
+        Me.btnGroup.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGroup.Location = New System.Drawing.Point(2, 52)
+        Me.btnGroup.Name = "btnGroup"
+        Me.btnGroup.Size = New System.Drawing.Size(385, 425)
+        Me.btnGroup.TabIndex = 11
+        Me.btnGroup.TabStop = False
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnPlay.BackColor = System.Drawing.Color.Thistle
+        Me.btnPlay.Font = New System.Drawing.Font("Arial Rounded MT Bold", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlay.Location = New System.Drawing.Point(132, 479)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(126, 45)
+        Me.btnPlay.TabIndex = 10
+        Me.btnPlay.Text = "START"
+        Me.btnPlay.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Plum
+        Me.ClientSize = New System.Drawing.Size(389, 526)
+        Me.Controls.Add(Me.btnGroup)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tic Tac Toe"
+        Me.btnGroup.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnGroup As GroupBox
+    Friend WithEvents btnPlay As Button
+End Class
---
+++ TICTACTOE/TICTACTOE/Form1.vb
@@ -0,0 +1,187 @@
+﻿Public Class Form1
+
+    Dim a As Integer = 0
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn1.Text = "0"
+        Else
+            btn1.Text = "X"
+        End If
+        btn1.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn2.Text = "0"
+        Else
+            btn2.Text = "X"
+        End If
+        btn2.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn3.Text = "0"
+        Else
+            btn3.Text = "X"
+        End If
+        btn3.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn4.Text = "0"
+        Else
+            btn4.Text = "X"
+        End If
+        btn4.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn5.Text = "0"
+        Else
+            btn5.Text = "X"
+        End If
+        btn5.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn6.Text = "0"
+        Else
+            btn6.Text = "X"
+        End If
+        btn6.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn7.Text = "0"
+        Else
+            btn7.Text = "X"
+        End If
+        btn7.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn8.Text = "0"
+        Else
+            btn8.Text = "X"
+        End If
+        btn8.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        a += 1
+        If a Mod 2 = 0 Then
+            btn9.Text = "0"
+        Else
+            btn9.Text = "X"
+        End If
+        btn9.Enabled = False
+        Winner()
+    End Sub
+
+    Private Sub Winner()
+        Dim dialogResult1 
+        If btn1.Text.Equals("X") And btn2.Text.Equals("X") And btn3.Text.Equals("X") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn4.Text.Equals("X") And btn5.Text.Equals("X") And btn6.Text.Equals("X") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn7.Text.Equals("X") And btn8.Text.Equals("X") And btn9.Text.Equals("X") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn1.Text.Equals("X") And btn4.Text.Equals("X") And btn7.Text.Equals("X") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn2.Text.Equals("X") And btn5.Text.Equals("X") And btn8.Text.Equals("X") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn3.Text.Equals("X") And btn6.Text.Equals("X") And btn9.Text.Equals("X") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn1.Text.Equals("X") And btn5.Text.Equals("X") And btn9.Text.Equals("X") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn3.Text.Equals("X") And btn5.Text.Equals("X") And btn7.Text.Equals("X") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn1.Text.Equals("0") And btn2.Text.Equals("0") And btn3.Text.Equals("0") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER 0 Wins")
+        ElseIf btn4.Text.Equals("0") And btn5.Text.Equals("0") And btn6.Text.Equals("0") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER 0 Wins")
+        ElseIf btn7.Text.Equals("0") And btn8.Text.Equals("0") And btn9.Text.Equals("0") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER 0 Wins")
+        ElseIf btn1.Text.Equals("0") And btn4.Text.Equals("0") And btn7.Text.Equals("0") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER 0 Wins")
+        ElseIf btn2.Text.Equals("0") And btn5.Text.Equals("0") And btn8.Text.Equals("0") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER X Wins")
+        ElseIf btn3.Text.Equals("0") And btn6.Text.Equals("0") And btn9.Text.Equals("0") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER 0 Wins")
+        ElseIf btn1.Text.Equals("0") And btn5.Text.Equals("0") And btn9.Text.Equals("0") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER 0 Wins")
+        ElseIf btn3.Text.Equals("0") And btn5.Text.Equals("0") And btn7.Text.Equals("0") Then
+            Dim x = a Mod 2
+            dialogResult1 = MessageBox.Show("PLAYER 0 Wins")
+        ElseIf a = 9 Then
+            dialogResult1 = MessageBox.Show("No Winner, DRAW")
+        End If
+
+        If dialogResult1 = DialogResult.OK Then
+            Restart()
+        End If
+    End Sub
+
+    Private Sub Restart()
+        a = 0
+        btn1.Text = ""
+        btn2.Text = ""
+        btn3.Text = ""
+        btn4.Text = ""
+        btn5.Text = ""
+        btn6.Text = ""
+        btn7.Text = ""
+        btn8.Text = ""
+        btn9.Text = ""
+    End Sub
+
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        btn1.Enabled = True
+        btn2.Enabled = True
+        btn3.Enabled = True
+        btn4.Enabled = True
+        btn5.Enabled = True
+        btn6.Enabled = True
+        btn7.Enabled = True
+        btn8.Enabled = True
+        btn9.Enabled = True
+    End Sub
+End Class
